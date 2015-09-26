5ecd33ed251c3a3138aae42b33c5592c2daccf87
Bloodyludi <ludwig@oberchef.de>
2015-09-26 19:26:52 +0200

    - created CoroutineRunner

---
+++ Assets/Framework/Extensions/CoroutineRunner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Container.Framework.Extensions
+{
+    public interface ICoroutineRunner
+    {
+        Coroutine StartCoroutine(IEnumerator routine);
+    }
+
+    public class CoroutineRunnerBehaviour : MonoBehaviour, ICoroutineRunner
+    {
+
+    }
+
+    public class CoroutineRunner : ICoroutineRunner
+    {
+        private readonly IGameObjectFactory gameObjectFactory;
+        private readonly ICoroutineRunner coroutineRunnerBehaviour;
+
+        public CoroutineRunner(IGameObjectFactory gameObjectFactory)
+        {
+            this.gameObjectFactory = gameObjectFactory;
+
+            var go = this.gameObjectFactory.Create("CoroutineRunnerPrefab");
+            coroutineRunnerBehaviour = go.GetComponent<ICoroutineRunner>();
+        }
+
+        Coroutine StartCoroutine(IEnumerator routine)
+        {
+            return coroutineRunnerBehaviour.StartCoroutine(routine);
+        }
+    }
+}
+
